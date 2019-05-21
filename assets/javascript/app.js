d5055627a8918f07edc94adc8bfeb3666dbaa41b
CiSigEp <cisigep@gmail.com>
2019-05-21 11:38:37 -0500

    question setup logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,5 @@
 var game;
-var questions = [];
 
-// Map for setting up questions of the specified type. 
-var questionTypes = {
-    trueFalse: {
-        setup(question, gameObject) {
-
-        }
-    },
-
-    multipleChoice: {
-        setup(question, gameObject) {
-
-        }
-    }
-}
 
 $(function() {
     game = {
@@ -27,7 +12,95 @@ $(function() {
         runningTimer: false,
         interval: 0,
 
+        questions: [
+            {
+                type: "trueFalse",
+                displayType: "True/False",
+                question: "This is a true/fase type question",
+                timeGiven: 15,
+                correctAnswer: 0
+            },
+            {
+                type: "multipleChoice",
+                displayType: "Multiple Choice",
+                question: "This is a multiple choice type question",
+                timeGiven: 15,
+                correctAnswer: 0,
+                answers: ["one", "two", "three", "four"]
+            }
+        ],
+
+        // Map for setting up questions of the specified type. 
+        questionTypes: {
+            trueFalse: {
+                setup(question, gameObject) {
+                    // Set up variables
+                    $('#questionType').text(question.displayType);
+                    $('#question').text(question.question);
+                    gameObject.setTimer(question.timeGiven);
+                    gameObject.setCorrect(question.correctAnswer);
+
+                    // Get the answer area and clear it
+                    var answerArea = $('#answers');
+                    answerArea.empty();
+
+                    var trueBox = $('<div>');
+                    var falseBox = $('<div>');
+
+                    // Set up answers for comparison
+                    trueBox.data("which", 0);
+                    falseBox.data("which", 1);
+
+                    trueBox.addClass('answer-box col-6');
+                    falseBox.addClass('answer-box col-6');
+                    trueBox.text('True');
+                    falseBox.text('False');
+
+                    answerArea.append(trueBox, falseBox);
+
+                    // Add click event to the boxes.
+                    $('.answer-box').click(function(){
+                        gameObject.checkAnswer($(this).data('which'));
+                    });
+
+                    gameObject.startInterval();
+                }
+            },
+        
+            multipleChoice: {
+                setup(question, gameObject) {
+                    $('#questionType').text(question.displayType);
+                    $('#question').text(question.question);
+                    gameObject.setTimer(question.timeGiven);
+                    gameObject.setCorrect(question.correctAnswer);
+
+                    var answerArea = $('#answers');
+                    answerArea.empty();
+
+                    for(var i = 0; i < question.answers.length; i++){
+                        var answerBox = $('<div>');
+                        answerBox.text(question.answers[i]);
+                        answerBox.addClass('answer-box col-12');
+                        answerBox.data('which', i);
+
+                        answerArea.append(answerBox);
+                    }
+
+                    // Add click event to the boxes.
+                    $('.answer-box').click(function(){
+                        gameObject.checkAnswer($(this).data('which'));
+                    });
+
+                    gameObject.startInterval();
+                }
+            }
+        },
+
         // Functions
+
+        questionSetup(test) {
+            this.questionTypes[this.questions[test].type].setup(this.questions[test], this);
+        },
     
         // Start the timer if it isn't already running
         startInterval() {
@@ -73,11 +146,15 @@ $(function() {
             $('.results-block').toggleClass('d-none');
         },
 
-        // Player gave incorrect answer
-        incorrect() {
-            this.numberIncorrect++;
+        checkAnswer(answer){
+            this.stopInterval()
+            if(answer === this.correct)
+                this.numberCorrect++;
+            else
+                this.numberIncorrect++;
         },
 
+        // Player timed out
         timeout() {
             this.numberUnanswered++;
         },
@@ -93,12 +170,12 @@ $(function() {
 
     };
 
-    $('#toggler').click(function() {
-        game.toggleView();
+    $('#truth').click(function(){
+        game.questionSetup(0);
     });
 
-    $('#results').click(function() {
-        game.showResults();
-    })
+    $('#multi').click(function(){
+        game.questionSetup(1);
+    });
 
 });
